e731b29b2b6666d8f415dba44be95581ccdba1f7
SupineDread <hazielfe@gmail.com>
2017-01-22 17:31:52 -0600

    cosas

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,7 +1,5 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModuleNgFactory } from './app.module.ngfactory';
+import { AppModule } from './app.module';
 
-enableProdMode();
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
+platformBrowserDynamic().bootstrapModule(AppModule);
